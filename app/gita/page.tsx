85a5f6a4b516ee57f745a48bd5e294ef73ed45eb
Lakshay Chopra <lakshay@ranadev.io>
2025-02-27 22:11:07 +0530

    fix: footer links

--- app/gita/page.tsx
+++ app/gita/page.tsx
@@ -80,22 +80,6 @@ export default async function Home() {
                   height={24}
                 />
               </a>
-              <a href="#" aria-label="Facebook">
-                <Image
-                  src="/facebook.png"
-                  alt="Facebook"
-                  width={24}
-                  height={24}
-                />
-              </a>
-              <a href="#" aria-label="WhatsApp">
-                <Image
-                  src="/whatsapp.png"
-                  alt="WhatsApp"
-                  width={24}
-                  height={24}
-                />
-              </a>
             </div>
           </div>
         </footer>
